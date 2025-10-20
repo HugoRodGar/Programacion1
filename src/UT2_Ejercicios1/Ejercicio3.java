e78b47995d0c37353a36b15e786ab342dbb9f68f
HugoRodGar <conbigames@gmail.com>
2025-10-20 17:44:16 +0200

    Añadido ejercicio3 de los ejercicios UT2

---
+++ src/UT2_Ejercicios1/Ejercicio3.java
@@ -0,0 +1,40 @@
+package UT2_Ejercicios1;
+
+import java.util.Scanner;
+
+public class Ejercicio3 {
+
+    static void main() {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduce un numero para saver a que numero de la semana corresponde");
+        int numero = sc.nextInt();
+
+        switch (numero) {
+            case 1:
+                System.out.println("El numero " + numero + " corresponde al Lunes");
+                break;
+            case 2:
+                System.out.println("El numero " + numero + " corresponde al Martes");
+                break;
+            case 3:
+                System.out.println("El numero " + numero + " corresponde al Miércoles");
+                break;
+            case 4:
+                System.out.println("El numero " + numero + " corresponde al Jueves");
+                break;
+            case 5:
+                System.out.println("El numero " + numero + " corresponde al Viernes");
+                break;
+            case 6:
+                System.out.println("El numero " + numero + " corresponde al Sábado");
+                break;
+            case 7:
+                System.out.println("El numero " + numero + " corresponde al Domingo");
+                break;
+            default:
+                System.out.println("El numero " + numero + " es un numero invalido, el numero introducido deve estar entre el 1 y el 7");
+        }
+
+    }
+}
