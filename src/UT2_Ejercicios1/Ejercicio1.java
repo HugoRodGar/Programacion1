61d853b88a5df1edbea41400aa75082f0c2b47e7
HugoRodGar <conbigames@gmail.com>
2025-10-20 17:33:20 +0200

    Añadido ejercicio1 de los ejercicios UT2

---
+++ src/UT2_Ejercicios1/Ejercicio1.java
@@ -0,0 +1,22 @@
+package UT2_Ejercicios1;
+
+import java.util.Scanner;
+
+public class Ejercicio1 {
+
+    static void main() {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduce un numero para calcular si es par o inpar");
+        double numero = sc.nextDouble();
+
+        if (numero % 2 != 0) {
+            System.out.println("El numero es inpar");
+        } else {
+            System.out.println("El numero es par");
+        }
+
+        sc.close();
+
+    }
+}
