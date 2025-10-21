3a7c0f66dfcb7f67c8ceb89590bcf82418c8e187
RGH <hugo.rodgar.8@educa.jcyl.es>
2025-10-21 19:15:29 +0200

    Añadido y modificado ejercicios 5 y 11 UT2 Ejercicios1

--- src/UT2_Ejercios2/Ejercicio11.java
+++ src/UT2_Ejercios2/Ejercicio11.java
@@ -9,11 +9,18 @@ public class Ejercicio11 {
         Scanner sc = new Scanner(System.in);
         System.out.println("Introduce un numero e intenta acertar");
         int numMin = 0;
-        int numMax = 1000000;
+        int numMax = 1000;
 
         int suNumero = (int) (Math.random() * (numMax - numMin + 1) + numMin);
         for (int i = numMin; i <= numMax; i++) {
-            int tuNumero;
+            int tuNumero = sc.nextInt();
+            if (tuNumero == suNumero) {
+                System.out.println("Has Acertado!!!!");
+            } else if (tuNumero < suNumero) {
+                System.out.println("Intentelo con un numero mas grande");
+            } else {
+                System.out.println("Intentelo con un numero mas pequeño");
+            }
         }
     }
 }
--- src/UT2_Ejercios2/Ejercicio5.java
+++ src/UT2_Ejercios2/Ejercicio5.java
@@ -4,19 +4,18 @@ import java.util.Scanner;
 
 public class Ejercicio5 {
 
-    static void main() {
+    public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
         System.out.println("Introduce un numero");
         int n = sc.nextInt();
 
-        for (int i = 1; i < n; i++) {
+        for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= i; j++) {
                 System.out.print(j + " ");
             }
             System.out.println();
         }
-
 
 
         sc.close();
