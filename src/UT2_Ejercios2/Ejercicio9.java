dbf9b40d3ba98441eb51fd6c475afe46242ebcff
HugoRodGar <conbigames@gmail.com>
2025-10-21 21:11:29 +0200

    Wip ejercicio9 de los ejercicios2 UT2

---
+++ src/UT2_Ejercios2/Ejercicio9.java
@@ -0,0 +1,27 @@
+package UT2_Ejercios2;
+
+import java.util.Scanner;
+
+public class Ejercicio9 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Introduce el número de términos (N): ");
+        int N = sc.nextInt();
+        int a = 0;
+        int b = 1;
+
+        System.out.println("Los " + N + " primeros términos de la serie de Fibonacci son:");
+        for (int i = 1; i <= N; i++) {
+            System.out.print(a + " ");
+
+            int siguiente = a + b;
+            a = b;
+            b = siguiente;
+        }
+
+        sc.close();
+
+    }
+}
+
