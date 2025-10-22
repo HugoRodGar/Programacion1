49d788f0437230365b640fc13f4f2de8682defac
RGH <hugo.rodgar.8@educa.jcyl.es>
2025-10-22 17:11:46 +0200

    Añadido ejercicio 1 EjerciciosDeFunciones

---
+++ src/EjerciciosDeFunciones/Ejercicio1.java
@@ -0,0 +1,14 @@
+package EjerciciosDeFunciones;
+
+public class Ejercicio1 {
+
+    public static void main(String[] args) {
+
+        saludar();
+
+    }
+
+    static void saludar() {
+        System.out.println("Hola");
+    }
+}
