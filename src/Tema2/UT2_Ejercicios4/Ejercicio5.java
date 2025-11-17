0918ab05685e01da8dfcefd4747b50c0921175d4
RGH <hugo.rodgar.8@educa.jcyl.es>
2025-11-17 20:04:58 +0100

    Carpetas y ejercicios ordenados

---
+++ src/Tema2/UT2_Ejercicios4/Ejercicio5.java
@@ -0,0 +1,14 @@
+package Tema2.UT2_Ejercicios4;
+
+public class Ejercicio5 {
+
+    public static void main(String[] args) {
+
+        System.out.println("");
+
+        for (int i; )  {
+
+        }
+    }
+
+}
