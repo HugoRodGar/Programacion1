74379df0efa483e2f394a9a9fd2b998f90e14fc7
RGH <hugo@gmail.com>
2026-01-19 18:55:39 +0100

    Cambios y ejercicios añadidos

---
+++ src/Gestion_de_inventario/Producto.java
@@ -0,0 +1,57 @@
+package Gestion_de_inventario;
+
+public class Producto {
+
+    private String nombre;
+    private double precio;
+    private int stock;
+
+    public Producto(String nombre, double precio, int stxock) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.stock = stxock;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public void vender(int cantidad) {
+        if (stock > cantidad) {
+            stock -= cantidad;
+            System.out.println("La compra se ha realizado con exito, Stock restante: " + stock);
+        } else {
+            System.out.println("Stock insuficiente para realizar la compra");
+        }
+    }
+
+    public void reponer(int cantidad) {
+        stock += cantidad;
+        System.out.println("El stock se ha aumentado en " + cantidad + ",  Stock actual: " + stock);
+    }
+
+    public void mostarInfo() {
+        System.out.printf("Nombre: " + nombre + "| Precio: " + precio + "| Stock: " + stock;
+    }
+
+}
